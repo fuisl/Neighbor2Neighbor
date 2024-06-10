af1f6ac37fc53b948d65f6a2a82ff98cf795d402
fuisloy <135719107+fuisl@users.noreply.github.com>
2024-06-10 13:13:55 +0700

    backed up code.

--- infer.py
+++ infer.py
@@ -9,21 +9,26 @@ from torchvision.utils import save_image
 import os
 import glob
 
+device = 'cuda:1'
 
 # Load the model
 model = UNet(1, 1, 96, True)
 
+path = 'checkpoint/nbr2nbr/lightning_logs/version_6/checkpoints/epoch=49-val_loss=0.0011.ckpt'
+checkpoint = torch.load(path, map_location='cuda:1')
+new_state_dict = {key.replace("model.", ""): value for key, value in checkpoint['state_dict'].items()}
+model.load_state_dict(new_state_dict)
 # Load the weights
-model.load_state_dict(torch.load('results/unet_n2n/2024-05-23-14-13/best.pth'))
+# model.load_state_dict(torch.load('results/unet_n2n/2024-05-23-14-13/best.pth'))
 model.eval()
-model.cuda()
+model.to(device)
 
 
 # Inference
 from data import ImageDataset_Val
 from torch.utils.data import DataLoader
 
-train_dataset = ImageDataset_Val('dataset/train')
+train_dataset = ImageDataset_Val('/data1tb/trietdao/dataset_OCT/self_fusion_1c')
 train_loader = DataLoader(dataset=train_dataset,
                           num_workers=8,
                           batch_size=1, 
@@ -32,11 +37,11 @@ train_loader = DataLoader(dataset=train_dataset,
                           drop_last=False)
 
 # inference loop
-output_base = 'results/unet_n2n/2024-05-23-14-13/infer'
+output_base = 'results/unet_n2n/lightning_logs/version_6/infer'
 monitor = tqdm(train_loader, total=len(train_loader), desc='Infer')
 with torch.no_grad():
     for image, path in monitor:
-        image = image.cuda()
+        image = image.to(device)
         out = model(image)
 
         # compute save path
